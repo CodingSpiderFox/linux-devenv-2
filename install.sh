26dd816d3cf843ffd9d5fb04050a9e7d25a38101
CodingSpiderFox <sunrisechain@gmail.com>
2021-09-22 20:51:45 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,7 +18,6 @@ sudo usermod -aG docker user
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 
 ### Install otpclient
 
